bc43180765c1b1bfcfc8487581a7dbb4baa66552
Arttu Savolainen <arttu.a.savolainen@tuni.fi>
2025-12-08 19:28:03 +0200

    Changed password to the flag in atk.sh for wireshark task

--- 03-snort/atk.sh
+++ 03-snort/atk.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-HOST=192.168.33.10 #ip
+HOST=192.168.0.10 #ip
 PORT=8000 #port
 INTERVAL=2 #seconds
 USER="ubuntu" #ssh username. User that flag will be sent
 NUM=0; #starting
 MAX_NUM=5; #end
-USERNAME=username; #username for website
-PASSWORD=p4ssw0rd; #password for website
+USERNAME=KalmaNVakinen; #username for website
+PASSWORD=CTF{FromBeyondTheGrave}; #password for website
 PASSNUM=1; #adds number to end of password
 
 while (( NUM<MAX_NUM ));do
-PASSNUM=$((PASSNUM+1))	
+PASSNUM=$((PASSNUM+1))
 POST_OUT=$(curl -s -H "Content-Type: application/x-www-form-urlencoded"  --data "username=$USERNAME&password=$PASSWORD$PASSNUM" "http://$HOST:$PORT/api/login" )
 POST_EXIT=$?
 if [ $POST_EXIT -eq 0 ];then
-	echo "Post Completed id:$POST_EXIT";
+        echo "Post Completed id:$POST_EXIT";
 else
-	#change this
-	#when wrong password
-	if echo "$POST_OUT" | grep -qi "Permission denied"; then
-		echo "Permission denied $NUM"
-	#when blocked
-	elif (($POST_EXIT == 56));then
-		echo "Connection reset by peer $NUM"
-		NUM=$((NUM+1))
-	#error
-	else
-		echo "else id:$POST_EXIT"
-	
-	
-	
+        #change this
+        #when wrong password
+        if echo "$POST_OUT" | grep -qi "Permission denied"; then
+                echo "Permission denied $NUM"
+        #when blocked
+        elif (($POST_EXIT == 56));then
+                echo "Connection reset by peer $NUM"
+                NUM=$((NUM+1))
+        #error
+        else
+                echo "else id:$POST_EXIT"
+
+
+
 fi
 
 fi
